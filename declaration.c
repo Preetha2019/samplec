54630aa5fda909dd665d88402a3d7668d9952140
Preetha Selvam <preethaselvam1998@gmail.com>
2019-11-08 11:35:11 +0530

    created this example program from advanced c

---
+++ declaration.c
@@ -0,0 +1,21 @@
+long int lSum; 
+long int lSub;// Using ‘int’ is optional.
+long int lCount;
+void SumInt(
+int nItem)
+{
+lSum += (long)nItem;
+++lCount;
+}
+void SubInt(
+int nItem)
+{
+lSum -= (long)nItem;
+--lCount;
+}
+int Average()
+{
+int nReturn = 0;
+nReturn = (int)(lSum / lCount);
+return (nReturn);
+}
