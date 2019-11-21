8a3f24bc4348cca229dcfdf246bcb34a97524ab3
Preetha Selvam <preethaselvam1998@gmail.com>
2019-11-21 15:59:18 +0530

    This sample.c program created for practice

---
+++ sample.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+void function();//funtion declaration
+void main()
+{
+    int a=0, c=0;
+    function();
+}
+void function()
+{
+    int a=0;
+    printf("\n Enter the value:\n");
+    scanf("%d",&a);
+}
