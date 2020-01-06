d1c254af951d16fbfb6be060e5a70c7f93d11342
Preetha2019 <57024934+Preetha2019@users.noreply.github.com>
2020-01-06 15:34:31 +0530

    Delete twofile.h

--- twofile.h
+++
@@ -1,5 +0,0 @@
-#include <stdio.h> // Make includes first part of file
-#include <string.h> // For string functions.
-#include <stdlib.h> // Standard include items.
-#include <process.h> // For exit(), etc.
-#include "define.h"
