6e794fdf4bef74575d1983634707622ee41be46d
Vairakkumar Vairaperumal <22436725+vairamsvsjdo@users.noreply.github.com>
2020-01-05 19:40:57 -0800

    update adsorting.c with c/c++ formatted code
    This is a sample code formatted using c/c++ tools extension in vscode.
    Used keys ctrl+K ctrl+f after selecting the entire file content to format the entire selected content

--- adsorting.c
+++ adsorting.c
@@ -1,15 +1,15 @@
-#include <stdio.h> // Make includes first part of file
-#include <string.h> // For string functions
+#include <stdio.h>   // Make includes first part of file
+#include <string.h>  // For string functions
 #include <process.h> // For exit(), etc.
-#include <malloc.h> // For malloc(), calloc(), realloc(), free()
-#include <search.h> // For qsort()...
-int main(void); // Define main() and the fact that this program
+#include <malloc.h>  // For malloc(), calloc(), realloc(), free()
+#include <search.h>  // For qsort()...
+int main(void);      // Define main() and the fact that this program
 // does not use any passed parameters
-int cmp(const void*, const void*);
+int cmp(const void *, const void *);
 #define MAX_CHARACTERS 32767 /* Total maximum characters */
-#define MAX_LINES 15500 /* Total maximum lines */
-#define BIGEST_LINE 512 /* Largest line readable from keyboard */
-#define MAX_BLOCKS 128 /* Allow 128 * MAX_CHARACTERS of memory */
+#define MAX_LINES 15500      /* Total maximum lines */
+#define BIGEST_LINE 512      /* Largest line readable from keyboard */
+#define MAX_BLOCKS 128       /* Allow 128 * MAX_CHARACTERS of memory */
 /* Although these variables are defined as external, they could
 * be defined inside the function or allocated dynamically,
 * depending on the program’s needs and the available memory.
@@ -23,74 +23,74 @@ int nBufferPointer = {MAX_CHARACTERS};
 int nLine = 0;
 int main()
 {
-int i;
-/* Use fprintf(stderr...) to force prompts and error messages
+    int i;
+    /* Use fprintf(stderr...) to force prompts and error messages
 * to be displayed on the user’s screen regardless of whether
 * the output has been redirected.
 */
-fprintf(stderr,
-"\n"
-"Peter's SORTFILE: Sorts large files at the speed of light!\n"
-"\n"
-" syntax: \n"
-" sortfile <inputfile >outputfile \n"
-"\n"
-" where: \n"
-" the program’s I/O is redirected\n\n");
-fprintf(stderr, "Reading input...\n");
-while (gets(szInput))
-{
-if ((nBufferPointer + strlen(szInput)) > MAX_CHARACTERS)
-{ // The line won’t fit! Allocate new memory:
-szBuffer = (char *)malloc(MAX_CHARACTERS);
-fprintf(stderr, " Allocating buffer (32K).\n");
-nBufferPointer = 0;
-pBlocks[nCurrentBlock] = szBuffer;
-++nCurrentBlock;
-if (szBuffer == NULL)
-{
-fprintf(stderr, "System sort memory exceeded, can't \
+    fprintf(stderr,
+            "\n"
+            "Peter's SORTFILE: Sorts large files at the speed of light!\n"
+            "\n"
+            " syntax: \n"
+            " sortfile <inputfile >outputfile \n"
+            "\n"
+            " where: \n"
+            " the program’s I/O is redirected\n\n");
+    fprintf(stderr, "Reading input...\n");
+    while (gets(szInput))
+    {
+        if ((nBufferPointer + strlen(szInput)) > MAX_CHARACTERS)
+        { // The line won’t fit! Allocate new memory:
+            szBuffer = (char *)malloc(MAX_CHARACTERS);
+            fprintf(stderr, " Allocating buffer (32K).\n");
+            nBufferPointer = 0;
+            pBlocks[nCurrentBlock] = szBuffer;
+            ++nCurrentBlock;
+            if (szBuffer == NULL)
+            {
+                fprintf(stderr, "System sort memory exceeded, can't \
 sort.\n");
-exit(16);
-}
-}
-pBuffer[nLine] = &szBuffer[nBufferPointer];
-strcpy(pBuffer[nLine], szInput);
-// The + 1 skips over the terminating NULL in each string.
-nBufferPointer += strlen(szInput) + 1;
-if (++nLine >= MAX_LINES)
-{ // Too many lines! End the program.
-fprintf(stderr, "Too many lines—cannot sort.\n");
-exit(16);
-}
-}
-//
-// Now sort the input lines
-//
-fprintf(stderr, "Sorting, %d lines, in %d buffers.\n",
-nLine,
-nCurrentBlock);
-qsort((void *)pBuffer,
-(size_t)nLine,
-sizeof(char *),
-cmp);
-fprintf(stderr, "Writing output...\n");
-for (i = 0; i < nLine; i++)
-{
-printf("%s\n", pBuffer[i]);
-}
-fprintf(stderr, "\n");
-for (i = 0; i < nCurrentBlock; i++)
-{
-free(pBlocks[i]);
-}
-return (0);
+                exit(16);
+            }
+        }
+        pBuffer[nLine] = &szBuffer[nBufferPointer];
+        strcpy(pBuffer[nLine], szInput);
+        // The + 1 skips over the terminating NULL in each string.
+        nBufferPointer += strlen(szInput) + 1;
+        if (++nLine >= MAX_LINES)
+        { // Too many lines! End the program.
+            fprintf(stderr, "Too many lines—cannot sort.\n");
+            exit(16);
+        }
+    }
+    //
+    // Now sort the input lines
+    //
+    fprintf(stderr, "Sorting, %d lines, in %d buffers.\n",
+            nLine,
+            nCurrentBlock);
+    qsort((void *)pBuffer,
+          (size_t)nLine,
+          sizeof(char *),
+          cmp);
+    fprintf(stderr, "Writing output...\n");
+    for (i = 0; i < nLine; i++)
+    {
+        printf("%s\n", pBuffer[i]);
+    }
+    fprintf(stderr, "\n");
+    for (i = 0; i < nCurrentBlock; i++)
+    {
+        free(pBlocks[i]);
+    }
+    return (0);
 }
 int cmp(const void *a, const void *b)
 {
-    const char* pa = *(const char**)a;
-    const char* pb = *(const char**)b;
-    int n1=atoi(pa);
-    int n2=atoi(pb);
-return ((n1>n2)) - (n1<n2);
+    const char *pa = *(const char **)a;
+    const char *pb = *(const char **)b;
+    int n1 = atoi(pa);
+    int n2 = atoi(pb);
+    return ((n1 > n2)) - (n1 < n2);
 }
