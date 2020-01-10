632443be1973df57548c9688ac6363072ef5b53d
Preetha Selvam <preethaselvam1998@gmail.com>
2020-01-11 00:07:02 +0530

    Updated step 1 for sorting program

---
+++ sortstep1readfile.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    fprintf(stderr,
+    "Firestone's law of forecasting\n"
+    "PDF document designated page\n"
+    "output for lond pictures, easy to share\n");
+    fprintf(stderr, "Reading Input:\n");
+    return 0;
+}
